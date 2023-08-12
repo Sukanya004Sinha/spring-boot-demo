3d084b83c4990a618bf169685c562f83d0627d09
sukanya004sinha <sukanyasinha004@gmail.com>
2023-08-12 16:56:38 +0530

    initial commit

---
+++ demo-spring-boot/src/main/java/com/springbootdemo/demospringboot/controller/DepartmentController.java
@@ -0,0 +1,19 @@
+package com.springbootdemo.demospringboot.controller;
+
+import com.springbootdemo.demospringboot.entity.Department;
+import com.springbootdemo.demospringboot.service.DepartmentService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class DepartmentController {
+    @Autowired
+    private DepartmentService departmentService;
+   @PostMapping("/departments")
+    public Department saveDepartment(@RequestBody Department department) {
+
+       return departmentService.saveDepartment(department);
+   }
+}
---
+++ demo-spring-boot/src/main/java/com/springbootdemo/demospringboot/service/DepartmentService.java
@@ -0,0 +1,7 @@
+package com.springbootdemo.demospringboot.service;
+
+import com.springbootdemo.demospringboot.entity.Department;
+
+public interface DepartmentService {
+    public Department saveDepartment(Department department);
+}
