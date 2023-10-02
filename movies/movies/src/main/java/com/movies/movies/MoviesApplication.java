3f6984a874e2e3007e0f2612c0baba332f92a111
sukanya004sinha <sukanyasinha004@gmail.com>
2023-10-02 16:29:35 +0530

    upto today

---
+++ movies/movies/src/main/java/com/movies/movies/MoviesApplication.java
@@ -0,0 +1,25 @@
+package com.movies.movies;
+
+import org.bson.types.ObjectId;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class MoviesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MoviesApplication.class, args);
+	}
+  @GetMapping("/")
+	public String apiRoot() {
+		return "Hello World!!";
+  }
+
+
+}
+
